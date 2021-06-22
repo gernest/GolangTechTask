ade8beb45e29a8b974dc3b2eb8162d59176ec1c5
gernest <geofreyernest@live.com>
2021-06-22 12:27:59 +0300

    add vote casting

--- cmd/voting_client/main.go
+++ cmd/voting_client/main.go
@@ -26,20 +26,51 @@ func main() {
 		os.Exit(1)
 	}
 	vo := api.NewVotingServiceClient(x)
-	talkToService(context.Background(), log, vo)
+	err = talkToService(context.Background(), log, vo)
+	if err != nil {
+		log.Error("Failed to talkToService", zap.Error(err))
+		os.Exit(1)
+	}
 }
 
 func talkToService(ctx context.Context, log *zap.Logger, x api.VotingServiceClient) error {
 	log.Info("CreateVoteable")
-	total := 10
+	total := 3
+	// we create and collect uuid for 3 voteables
+	var voteables []string
 	for i := 0; i < total; i++ {
-		_, err := x.CreateVoteable(ctx, &api.CreateVoteableRequest{
+		res, err := x.CreateVoteable(ctx, &api.CreateVoteableRequest{
 			Question: fmt.Sprintf("%d - Truth or Dare?", i),
 			Answers:  []string{"Truth", "Dare"},
 		})
 		if err != nil {
 			return err
 		}
+		voteables = append(voteables, res.Uuid)
+	}
+
+	log.Info("ListVoteables - list all voteables")
+	_, err := x.ListVoteables(ctx, &api.ListVoteableRequest{})
+	if err != nil {
+		return err
+	}
+
+	log.Info("ListVoteables - list with limit")
+	limit := 2
+	res, err := x.ListVoteables(ctx, &api.ListVoteableRequest{Limit: int32(limit)})
+	if err != nil {
+		return err
+	}
+
+	log.Info("ListVoteables - list with pagination")
+	_, err = x.ListVoteables(ctx, &api.ListVoteableRequest{LastIndex: res.LastIndex})
+	if err != nil {
+		return err
+	}
+	log.Info("CastVote")
+	_, err = x.CastVote(ctx, &api.CastVoteRequest{Uuid: voteables[0], AnswerIndex: 0})
+	if err != nil {
+		return err
 	}
 	return nil
 }
